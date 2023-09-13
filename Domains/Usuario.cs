829a1a679feedde82cc7b58861f684acad946721
enzoquarelo <enzo.quarelo@gmail.com>
2023-09-13 10:54:10 -0300

    Domains Finalizados

--- Domains/Usuario.cs
+++ Domains/Usuario.cs
@@ -1,6 +1,28 @@
-﻿namespace webapi.inlock.codeFirst.manha.Domains
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.inlock.codeFirst.manha.Domains
 {
-    public class UsuarioDomain
+    [Table("Usuario")]
+    public class Usuario
     {
+        [Key]
+        public Guid IdUsuario { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage = "Email do Usuário obrigatório!")]
+        public string? Email { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage = "Senha obrigatória!")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage ="Senha deve conter de 6 á 20 caractéres!")]
+        public string? Senha { get; set; }
+
+
+        [Required(ErrorMessage = "Informe o tipo desse usuário!")]
+        public Guid IdTipoDeUsuario { get; set; }
+        //instancia a propriedade do tipo estudio para fazer a referencia com a foreignKey de IdTipoDeUsuario
+        [ForeignKey("IdTipoDeUsuario")]
+        public TiposDeUsuario? TipoDeUsuario { get; set; }
     }
 }
